2e33353508d9ac30174aca99a04b09fa9a35eab0
Kai <henning.kai@web.de>
2024-09-22 12:21:16 +0200

    update gitignore

--- render_mesh_with_normals.py
+++ render_mesh_with_normals.py
@@ -3,6 +3,7 @@ import vtk_
 import geo
 import fileio
 
+
 filepath = sys.argv[1]
 vertices, triangles = fileio.load_mesh_with_libigl(filepath)
 vertex_normals = geo.compute_vertex_normals(vertices, triangles)
